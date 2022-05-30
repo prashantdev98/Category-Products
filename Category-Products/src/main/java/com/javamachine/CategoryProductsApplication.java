2531317f00a743ccf67638151dfbcea0e5ea62fe
prashantdev98 <105698467+prashantdev98@users.noreply.github.com>
2022-05-30 11:05:37 +0530

    Done Upload

---
+++ Category-Products/src/main/java/com/javamachine/CategoryProductsApplication.java
@@ -0,0 +1,13 @@
+package com.javamachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryProductsApplication.class, args);
+	}
+
+}
